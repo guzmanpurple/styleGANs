6a24da1265dedee6674b7d876420387b3b5587ee
guzmanpurple <92334502+guzmanpurple@users.noreply.github.com>
2023-04-06 14:22:20 -0400

    k
    k

---
+++ IM-Screens_2023_Load_Video_Basic_copy_style_GAN_2023_04_06_18_14_21/sketch.js
@@ -0,0 +1,40 @@
+
+
+let vid;
+let playing = true;
+
+function setup() {
+  createCanvas(1920, 1080);
+  // noCanvas();
+
+  vid = createVideo("Untitled.mp4");
+  vid.size(1920, 1080);
+  vid.volume(0);
+  vid.loop();
+  vid.hide(); // hides the html video loader
+  vid.position(0.0);
+
+}
+
+function draw() {
+  background(220);
+  let img = vid.get();
+  image(img, 0, 0); // redraws the video frame by frame in                           p5
+  //textSize(40);
+  //counter = nf(vid.time(), 0, 2); // first argument is decimal places to the left (use zero to default to places necessary)
+  //text(counter, 10, 300);
+}
+
+// function keyPressed() {
+//  vid.time(random(vid.duration())) 
+// }
+
+function mousePressed() {
+ if (playing) {
+   vid.pause();
+ }
+  else {
+    vid.play();
+  }
+  playing = !playing;
+}
